10f95d1751bfc594fed3039dc9b08abe971046e9
Fernando Gontijo <fernandolucasbd@gmail.com>
2018-08-08 21:00:07 -0300

    setup project with typeorm, graphql yoga, and tslint

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import "reflect-metadata";
+import { GraphQLServer } from 'graphql-yoga'
+// ... or using `require()`
+// const { GraphQLServer } = require('graphql-yoga')
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: (_: any, { name}: any) => `Hello ${name || 'World'}`,
+  },
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers })
+server.start(() => console.log('Server is running on localhost:4000'))
