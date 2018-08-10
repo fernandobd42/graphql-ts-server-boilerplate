f3fdbc45176c723ff34afb328983ab841b69a10a
Fernando Gontijo <fernandolucasbd@gmail.com>
2018-08-09 22:16:24 -0300

    split up resolvers and queries

---
+++ src/modules/register/resolvers.ts
@@ -0,0 +1,25 @@
+import * as bcrypt from 'bcryptjs'
+
+import { ResolverMap } from '../../types/graphql-utils'
+import { User } from '../../entity/User'
+
+export const resolvers: ResolverMap = {
+  Query: {
+    bye: () => "Bye"
+  },
+  Mutation: {
+    register: async (
+      _,
+      { email, password }: GQL.IRegisterOnMutationArguments
+    ) => {
+      const hashedPassword = await bcrypt.hash(password, 10)
+      const user = await User.create({
+        email,
+        password: hashedPassword
+      })
+
+      await user.save()
+      return true
+    }
+  }
+}
