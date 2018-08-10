44fb5a716724521b0faec6775856c7fbed0e7679
Fernando Gontijo <fernandolucasbd@gmail.com>
2018-08-09 21:31:02 -0300

    start server from jest

--- src/tests/register.test.ts
+++ src/tests/register.test.ts
@@ -1,11 +1,15 @@
 import { request } from 'graphql-request'
+
 import { host } from './constants'
 import { User } from '../entity/User'
+import { startServer } from './../startServer'
 
-import { CreateTypeOrmConnection } from './../utils/CreateTypeOrmConnection'
+let getHost = () => ''
 
 beforeAll(async () => {
-  await CreateTypeOrmConnection()
+  const app = await startServer()
+  const { port } = app.address()
+  getHost = () => `http://127.0.0.1:${port}`
 })
 
 const email = 'jao@gmail.com'
@@ -17,7 +21,7 @@ const mutation = `
     }
 `
 test('Register User', async () => {
-  const response = await request(host, mutation)
+  const response = await request(getHost(), mutation)
   expect(response).toEqual({ register: true })
   const users = await User.find({ where: { email } })
   expect(users).toHaveLength(1)
